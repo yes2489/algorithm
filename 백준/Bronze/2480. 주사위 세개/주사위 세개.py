7334bd2c73aed150497c62e1098c0c1b81831ea7
EunSeo Yang <yes24150@gmail.com>
2025-07-14 22:50:43 +0900

    [Bronze IV] Title: 주사위 세개, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,8 @@
+arr = list(map(int, input().split()))
+arr.sort()
+if(arr[0] == arr[1] == arr[2]):
+    print(10000+arr[0]*1000)
+elif((arr[0] == arr[1]) or (arr[1] == arr[2])):
+    print(1000+arr[1]*100)
+else:
+    print(arr[2]*100) 
