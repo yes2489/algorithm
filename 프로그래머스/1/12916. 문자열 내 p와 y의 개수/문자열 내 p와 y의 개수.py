72d3842d36bdaf51dbdef283c2a01608aa81b930
EunSeo Yang <yes24150@gmail.com>
2025-07-15 22:22:07 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.01 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
@@ -1,14 +1,2 @@
 def solution(s):
-    letter = s.lower()
-    p = 0
-    y = 0
-    for i in letter:
-        if i == 'p':
-            p += 1
-        elif i == 'y':
-            y += 1
-
-    if p == y or p == y == 0:
-        return True
-    else:
-        return False
+    return s.lower().count('p') == s.lower().count('y')
