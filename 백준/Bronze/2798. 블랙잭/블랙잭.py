f1d8ab134add9f38597dff78183e66da19a6a716
EunSeo Yang <yes24150@gmail.com>
2025-07-19 20:03:46 +0900

    [Bronze II] Title: 블랙잭, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,14 +1,14 @@
-import sys
-input = sys.stdin.readline
+from itertools import combinations
+
 N, M = map(int, input().split())
 card = list(map(int, input().split()))
 
-# 3장의 합이 M을 넘지 않으면서 M과 최대한 가까우면 출력
-max_total=0
-for i in range(len(card)):
-    for j in range(i+1, len(card)):
-        for k in range(j+1, len(card)):
-            total = card[i]+card[j]+card[k];
-            if total <= M:
-                max_total = max(max_total, total)
+# combinations: 조합을 자동으로 만들어주는 함수
+# combinations(card, 3) => card list에서 3장씩 뽑는 모든 조합
+max_total = 0
+for combo in combinations(card, 3):
+    total = sum(combo)
+    if total <= M:
+        max_total = max(max_total, total)
+
 print(max_total)
