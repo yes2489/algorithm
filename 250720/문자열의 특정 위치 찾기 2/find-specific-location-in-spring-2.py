417a70c3a35bc87b2121e9a23466ac96522e728d
EunSeo Yang <yes24150@gmail.com>
2025-07-20 16:08:24 +0900

    Title: 문자열의 특정 위치 찾기 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250720/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.py
+++ 250720/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.py
@@ -1,8 +1,20 @@
+# fruits = ['apple', 'banana', 'grape', 'bluberry', 'orange']
+# char = input()
+# cnt = 0
+# for fruit in fruits:
+#     if len(fruit) > 3 and (fruit[2] == char or fruit[3] == char):
+#         print(fruit)
+#         cnt += 1
+# print(cnt)
+
 fruits = ['apple', 'banana', 'grape', 'bluberry', 'orange']
+
 char = input()
 cnt = 0
-for fruit in fruits:
-    if len(fruit) > 3 and (fruit[2] == char or fruit[3] == char):
-        print(fruit)
-        cnt += 1
+
+for i in range(5):
+	if fruits[i][2] == char or fruits[i][3] == char:
+		print(fruits[i])
+		cnt += 1
+
 print(cnt)
