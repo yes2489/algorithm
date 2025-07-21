f6f01c1d4734e2ae7ed8f379b9b7f37e2c3782da
EunSeo Yang <yes24150@gmail.com>
2025-07-22 00:22:38 +0900

    Title: 배열의 합, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250722/배열의 합/sum-of-array.py
+++ 250722/배열의 합/sum-of-array.py
@@ -1,8 +1,4 @@
 board = [list(map(int, input().split())) for row in range(4)]
 
-total = 0
 for i in range(4):
-    for j in range(4):
-        total += board[i][j]
-    print(total)
-    total = 0
+    print(sum(board[i]))
