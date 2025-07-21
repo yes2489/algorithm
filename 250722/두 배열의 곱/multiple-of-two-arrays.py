55cff3c292ed5b24d361e94b142cda9b05786bf3
EunSeo Yang <yes24150@gmail.com>
2025-07-22 00:08:21 +0900

    Title: 두 배열의 곱, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250722/두 배열의 곱/multiple-of-two-arrays.py
@@ -0,0 +1,8 @@
+arr1 = [list(map(int, input().split())) for row in range(3)]
+input()
+arr2 = [list(map(int, input().split())) for row in range(3)]
+
+for i in range(3):
+    for j in range(3):
+        print(arr1[i][j] * arr2[i][j], end=' ')
+    print()
