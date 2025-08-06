12ba14bef3b7523e2c15858ac43b2646bc761b12
EunSeo Yang <yes24150@gmail.com>
2025-08-06 22:28:23 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 85 ms, Memory: 59,776 KB -BaekjoonHub

---
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
@@ -0,0 +1,15 @@
+# 10개의 테스트케이스
+for tc in range(10):
+    # 건물 수
+    n = int(input())
+    
+    # 건물의 높이
+    buildings = list(map(int, input().split()))
+    
+    res = 0
+    # 순회 하면서 내 양 옆이 비었는지 확인
+    for i in range(2, len(buildings)-1):
+        if buildings[i-2] < buildings[i] and buildings[i-1] < buildings[i] and buildings[i+1] < buildings[i] and buildings[i+2] < buildings[i]:
+            res += buildings[i] - max(buildings[i-1], buildings[i-2], buildings[i+1], buildings[i+2])
+            
+    print(f'#{tc+1} {res}')
