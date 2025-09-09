8a3ca97764b934490a50cb7c1ab0ecc96e822e0c
EunSeo Yang <yes24150@gmail.com>
2025-09-09 20:23:27 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34936 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,25 +1,30 @@
-import sys
 from collections import deque
+import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-board = [list(map(int, input().strip())) for _ in range(n)]
+n,m = map(int, input().split())
+board = []
+for i in range(n):
+    arr = list(map(int, input().strip()))
+    board.append(arr)
 
+dr = [1, 0, -1, 0]
+dc = [0, -1, 0 , 1]
+    
 q = deque()
-q.append((0, 0))
+q.append((0,0))
 
 while q:
-    r, c = q.popleft()
+    r, c = q.popleft()  
     
-    dr = [-1, 0, 1, 0]
-    dc = [0, 1, 0, -1]
-
+    if (r, c) == (n-1, m-1):
+        break
+    
     for i in range(4):
         nr = r + dr[i]
         nc = c + dc[i]
-
         if 0 <= nr < n and 0 <= nc < m and board[nr][nc] == 1:
-            board[nr][nc] = board[r][c] + 1
+            board[nr][nc] += board[r][c]
             q.append((nr, nc))
 
 print(board[n-1][m-1])
