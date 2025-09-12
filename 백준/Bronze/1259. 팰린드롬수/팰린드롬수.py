d3af6fb03b96736faf70143188f32113908923cf
EunSeo Yang <yes24150@gmail.com>
2025-09-12 22:20:29 +0900

    [Bronze I] Title: 팰린드롬수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,17 +1,10 @@
 while True:
-    n = list(map(int, input().strip()))
+    n = input().strip()
     
-    if n == [0]:
+    if n == "0":
         break
     
-    r_n = n[::-1]
-    
-    is_palindrome = 'yes'
-    
-    for i in range(len(n)):
-        if n[i] != r_n[i]:
-            is_palindrome = 'no'
-            break
-        
-    print(is_palindrome)
-    
+    if n == n[::-1]:
+        print('yes')
+    else:
+        print('no')
