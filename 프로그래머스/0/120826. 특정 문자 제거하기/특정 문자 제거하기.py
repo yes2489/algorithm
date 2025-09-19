7e57024e99e31c9f524af242f8bbe558d5f5735e
EunSeo Yang <yes24150@gmail.com>
2025-09-19 22:27:28 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,6 +1,2 @@
 def solution(my_string, letter):
-    answer = ''
-    for s in my_string:
-        if s != letter:
-            answer += s
-    return answer
+    return my_string.replace(letter, '')
