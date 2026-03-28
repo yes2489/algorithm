a94395bdb5abdcbe9bd1e76aacc1d22fcd56b304
EunSeo Yang <yes24150@gmail.com>
2026-03-28 21:41:57 +0900

    Title: 변수 값 복사하기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260328/변수 값 복사하기 2/copying-variable-values-2.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5, b = 6, c = 7;
+        a = b = c;
+        System.out.printf("%d %d %d", a, b, c);
+    }
+}
