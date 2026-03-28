b9619a6e74c1bcbfc771aba657740a03a0216ed1
EunSeo Yang <yes24150@gmail.com>
2026-03-28 21:58:13 +0900

    Title: 세 실수의 반올림, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260328/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble(), b = sc.nextDouble(), c = sc.nextDouble();
+        
+        System.out.printf("%.3f\n%.3f\n%.3f", a, b, c);
+    }
+}
