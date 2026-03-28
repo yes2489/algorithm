60ccfd8927852f9edb3410ac9546c4b3b863feb4
EunSeo Yang <yes24150@gmail.com>
2026-03-28 21:29:53 +0900

    Title: 변수 값 교체하기 5, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260328/변수 값 교체하기 5/replacing-variable-values-5.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 2;
+        int b = 6;
+        a = b;
+        System.out.printf("%d\n%d", a, b);
+    }
+}
