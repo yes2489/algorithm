2489f8563c7ecc6d5755f4f11d6eb36c77c6a878
EunSeo Yang <yes24150@gmail.com>
2026-03-28 22:00:33 +0900

    Title: 문자 받아 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260328/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+
+        System.out.println(c);
+    }
+}
