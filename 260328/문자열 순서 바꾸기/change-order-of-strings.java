a9ec70d40413984fca038faa1a4537275c1939b3
EunSeo Yang <yes24150@gmail.com>
2026-03-28 22:05:06 +0900

    Title: 문자열 순서 바꾸기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260328/문자열 순서 바꾸기/change-order-of-strings.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next(), t = sc.next();
+
+        System.out.printf("%s\n%s", t, s);
+    }
+}
