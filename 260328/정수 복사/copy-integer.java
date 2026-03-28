95b95fe6ecefe9e4117f8f9afbc8fa3302a38108
EunSeo Yang <yes24150@gmail.com>
2026-03-28 21:30:37 +0900

    Title: 정수 복사, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260328/정수 복사/copy-integer.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 3;
+        int b = 4;
+        b = a;
+        System.out.printf("%d %d\n%d", a, b, a*b);
+    }
+}
