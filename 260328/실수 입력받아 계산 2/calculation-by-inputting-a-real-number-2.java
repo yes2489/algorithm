0d9970811aed856c92b2fc90a3b2efc7958be3c3
EunSeo Yang <yes24150@gmail.com>
2026-03-28 21:51:23 +0900

    Title: 실수 입력받아 계산 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260328/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        
+        System.out.printf("%.2f", a + 1.5);
+    }
+}
