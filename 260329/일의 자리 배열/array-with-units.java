891de5e96add9985a946b4d294804cc8bd7e0422
EunSeo Yang <yes24150@gmail.com>
2026-03-29 20:58:03 +0900

    Title: 일의 자리 배열, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260329/일의 자리 배열/array-with-units.java
+++ 260329/일의 자리 배열/array-with-units.java
@@ -9,15 +9,9 @@ public class Main {
         arr[0] = sc.nextInt();
         arr[1] = sc.nextInt();
 
+        // 1의 자리로 채우기... 문제 제대로 읽기...
         for (int i = 2; i < 10; i++) {
-            arr[i] = arr[i-1] + arr[i-2];
-            if (arr[i] >= 10) {
-                while(true) {
-                    arr[i] %= 10;
-
-                    if (arr[i] < 10) break;
-                }
-            }
+            arr[i] = (arr[i-1] + arr[i-2]) % 10;
         }
 
         for (int val : arr) {
