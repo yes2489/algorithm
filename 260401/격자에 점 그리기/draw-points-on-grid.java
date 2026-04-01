9313583df85c01e9115e832210999e61664a7b7d
EunSeo Yang <yes24150@gmail.com>
2026-04-01 20:43:08 +0900

    Title: 격자에 점 그리기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260401/격자에 점 그리기/draw-points-on-grid.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(), m = sc.nextInt();
+        int[][] board = new int[n + 1][n + 1];
+        int num = 1;
+
+        for (int i = 0; i < m; i++) {
+            int r = sc.nextInt();
+            int c = sc.nextInt();
+
+            board[r][c] = num;
+            num++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
