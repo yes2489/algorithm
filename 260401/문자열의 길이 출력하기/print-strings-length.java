dbefcecde3017cf765515390655e0b8128cd5b31
EunSeo Yang <yes24150@gmail.com>
2026-04-01 20:50:12 +0900

    Title: 문자열의 길이 출력하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260401/문자열의 길이 출력하기/print-strings-length.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.next(), str2 = sc.next();
+
+        System.out.println(str1.length() + str2.length());
+    }
+}
