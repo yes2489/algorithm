f968535da54efc38102d3dcd1a674ef740fc6664
EunSeo Yang <yes24150@gmail.com>
2026-04-01 20:48:24 +0900

    Title: 문자열 두번 출력하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260401/문자열 두번 출력하기/print-string-twice.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        System.out.printf("%s\n%s", str, str);
+    }
+}
