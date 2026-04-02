72ad2e38fbcb7bd6fa3011ef2e9017922dcf6ef1
EunSeo Yang <yes24150@gmail.com>
2026-04-02 13:21:03 +0900

    Title: 단어로 구분하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/단어로 구분하기/separate-words-with-words.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(sc.next());
+        }
+    }
+}
