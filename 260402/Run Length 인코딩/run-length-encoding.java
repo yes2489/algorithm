92f23606e901eaf581781f2f8c822765b51bead0
EunSeo Yang <yes24150@gmail.com>
2026-04-02 14:05:20 +0900

    Title: Run Length 인코딩, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260402/Run Length 인코딩/run-length-encoding.java
+++ 260402/Run Length 인코딩/run-length-encoding.java
@@ -2,32 +2,26 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Please write your code here.
         Scanner sc = new Scanner(System.in);
 
         String a = sc.next();
         StringBuilder ans = new StringBuilder();
+
         int cnt = 1;
+        ans.append(a.charAt(0));
 
-        for (int i = 0; i < a.length(); i++) {
-            if (i == 0) {
-                ans.append(a.charAt(i));
-                continue;
-            }
-
-            if (a.charAt(i-1) == a.charAt(i)) {
+        for (int i = 1; i < a.length(); i++) {
+            if (a.charAt(i - 1) == a.charAt(i)) {
                 cnt++;
             } else {
                 ans.append(cnt);
                 ans.append(a.charAt(i));
                 cnt = 1;
             }
-
-            if (i == a.length() - 1) {
-                ans.append(cnt);
-                break;
-            }
         }
+
+        ans.append(cnt);
+
         System.out.println(ans.length());
         System.out.println(ans);
     }
