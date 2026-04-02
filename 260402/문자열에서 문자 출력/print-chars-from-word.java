ded45170d10dd78c80f53514628e53f09fae797b
EunSeo Yang <yes24150@gmail.com>
2026-04-02 13:35:01 +0900

    Title: 문자열에서 문자 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/문자열에서 문자 출력/print-chars-from-word.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+        }
+    }
+}
