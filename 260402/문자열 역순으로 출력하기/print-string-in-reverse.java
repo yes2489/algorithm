3652b24cba1840c5c19705ee9baac9e0982798f5
EunSeo Yang <yes24150@gmail.com>
2026-04-02 13:13:42 +0900

    Title: 문자열 역순으로 출력하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/문자열 역순으로 출력하기/print-string-in-reverse.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        String[] arr = new String[4];
+
+        for (int i = 0; i < 4; i++) {
+            arr[i] = sc.next();
+        }
+
+        for (int i = 3; i >= 0; i--) {
+            System.out.println(arr[i]);
+        }
+    }
+}
