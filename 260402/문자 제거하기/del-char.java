7f35d3492ff130f38309dbf5e3a3b34d00b200e6
EunSeo Yang <yes24150@gmail.com>
2026-04-02 17:34:45 +0900

    Title: 문자 제거하기, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260402/문자 제거하기/del-char.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        while(str.length() > 1) {
+            int idx = sc.nextInt();
+
+            if (idx > str.length()) {
+                str = str.substring(0, str.length() - 1);
+            } else {
+                str = str.substring(0, idx) + str.substring(idx + 1);
+            }
+
+            System.out.println(str);
+        }
+    }
+}
