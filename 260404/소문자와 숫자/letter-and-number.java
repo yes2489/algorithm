df5427028801cfdfd0ae53993cf884834ffdd67b
EunSeo Yang <yes24150@gmail.com>
2026-04-04 14:00:40 +0900

    Title: 소문자와 숫자, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260404/소문자와 숫자/letter-and-number.java
+++ 260404/소문자와 숫자/letter-and-number.java
@@ -9,10 +9,11 @@ public class Main {
 
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-            
-            if (Character.isUpperCase(c)) {
-                System.out.print(Character.toLowerCase(c));
-            } else if (Character.isLowerCase(c) || Character.isDigit(c)) {
+            if ('A' <= c && c <= 'Z') {
+                System.out.print(String.valueOf(c).toLowerCase());
+            } else if ('a' <= c && c <= 'z') {
+                System.out.print(c);
+            } else if ('0' <= c && c <= '9') {
                 System.out.print(c);
             }
         }
