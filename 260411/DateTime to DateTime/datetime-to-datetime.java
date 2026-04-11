9cb578f4827eceba87bf7f6cd319a0a2a9ab8c80
EunSeo Yang <yes24150@gmail.com>
2026-04-11 18:37:56 +0900

    Title: DateTime to DateTime, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260411/DateTime to DateTime/datetime-to-datetime.java
+++ 260411/DateTime to DateTime/datetime-to-datetime.java
@@ -1,35 +1,19 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
         int B = sc.nextInt();
         int C = sc.nextInt();
-        // Please write your code here.
-        if (A < 11 || (A == 11 && B < 11) || (A == 11 && B == 11 && C < 11)) {
+
+        int start = 11 * 24 * 60 + 11 * 60 + 11;
+        int end = A * 24 * 60 + B * 60 + C;
+
+        if (end < start) {
             System.out.println(-1);
-            return;
+        } else {
+            System.out.println(end - start);
         }
-
-        int ans = 0, date = 11, hour = 11, min = 11;
-
-        while(true) {
-            if (date == A && hour == B && min == C) {
-                break;
-            }
-
-            ans++;
-            min++;
-            if (min >= 60) {
-                min = 0;
-                hour++;
-                if (hour >= 24) {
-                    hour = 0;
-                    date++;
-                }
-            }
-        }
-
-        System.out.println(ans);
     }
 }
