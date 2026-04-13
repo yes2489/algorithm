e9f97297965ba6a54af245729160b7af78e096a4
EunSeo Yang <yes24150@gmail.com>
2026-04-13 14:14:44 +0900

    Title: 2진수로 변환하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260413/2진수로 변환하기/convert-to-binary.java
+++ 260413/2진수로 변환하기/convert-to-binary.java
@@ -7,11 +7,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
+        if (n == 0) {
+            System.out.println(0);
+            return;
+        }
+
         StringBuilder sb = new StringBuilder();
 
         while (n > 0) {
-            sb.append(n%2);
-            n /= 2;
+            sb.append(n & 1); // 마지막 비트 삽입
+            n = n >> 1; // 오른쪽으로 1칸 이동
         }
 
         System.out.println(sb.reverse());
