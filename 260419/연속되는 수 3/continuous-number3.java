06ca027bac85ea89dd03d40f65a4a221009dab17
EunSeo Yang <yes24150@gmail.com>
2026-04-19 21:27:45 +0900

    Title: 연속되는 수 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260419/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = 1;
+        int current = 1;
+
+        for (int i = 1; i < n; i++) {
+            if ((arr[i] > 0 && arr[i - 1] > 0) || (arr[i] < 0 && arr[i - 1] < 0)) {
+                current++;
+            } else {
+                current = 1;
+            }
+            max = Math.max(max, current);
+        }
+
+        System.out.println(max);
+    }
+}
