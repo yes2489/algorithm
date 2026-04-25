e11d7633826ab80943ac199788c759d0134d2ce4
EunSeo Yang <yes24150@gmail.com>
2026-04-25 16:56:29 +0900

    Title: 일렬로 서있는 소 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260425/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
+++ 260425/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -13,9 +13,9 @@ public class Main {
         int cnt = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                if (arr[i] < arr[j]) {
+                if (arr[i] <= arr[j]) {
                     for (int k = j + 1; k < n; k++) {
-                        if (arr[j] < arr[k])
+                        if (arr[j] <= arr[k])
                             cnt++;
                     }
                 }
