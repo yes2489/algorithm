7ab26a914c7c980e58e01416207a038c31d3895d
EunSeo Yang <yes24150@gmail.com>
2026-05-02 21:21:18 +0900

    [Passed] 구간 중 최대 합 (138ms, 10MB)

---
+++ codetree/trail2/구간 중 최대 합/max-sum-of-subarray.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i <= n - k; i++) {
+            int sum = 0;
+            for (int j = i; j < i + k; j++) 
+                sum += arr[j];
+            
+            max = Math.max(max, sum);
+        }
+
+        System.out.println(max);
+    }
+}
