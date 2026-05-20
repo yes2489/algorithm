ccb11f56d8b16d271c06f700acec5dc4631c23d3
EunSeo Yang <yes24150@gmail.com>
2026-05-20 13:41:55 +0900

    [Passed] 이상한 폭탄 2 (150ms, 11MB)

---
+++ codetree/trail2/이상한 폭탄 2/strange-bomb-2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int ans = -1;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j] && j - i <= k)
+                    ans = Math.max(ans, arr[i]);
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
